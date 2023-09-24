3598bce3e9958277a0b3f77cd206fe57a20c3ed3
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-09-23 17:45:24 -0700

    review.py

--- models/review.py
+++ models/review.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class Review(BaseModel):
     """ Review classto store review information """
 
-    __tablename__='reviews'
+    __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
--- models/state.py
+++ models/state.py
@@ -5,11 +5,13 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class State(BaseModel):
     """ State class """
-     __tablename__ = 'states'
+    __tablename__ = 'states'  # Corrected indentation here
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='delete', backref='state')
+
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
