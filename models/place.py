3050bd5942dd8c103550c430f7073c9c691c5af5
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-09-23 17:39:52 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -15,8 +15,8 @@ place_amenity = Table('place_amenity', Base.metadata,
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False),
-))
+                             primary_key=True, nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
