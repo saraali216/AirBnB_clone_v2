70855b49465dc47fedbf076639cd664d64c4f75f
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-09-23 19:08:49 -0700

    Q/a

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel, Base):
@@ -11,4 +12,6 @@ class Amenity(BaseModel, Base):
 
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+    # Define the primary key for the amenities table
+    id = Column(String(60), primary_key=True, nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity")
